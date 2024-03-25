82d492859591650691a168d7009cd03d2b2225dc
mingduan <mingduan@iflytek.com>
2024-03-25 17:01:18 +0800

    增加config

--- config/config.go
+++ config/config.go
@@ -2,14 +2,42 @@ package config
 
 import (
 	"fmt"
+	"github.com/spf13/viper"
 	"os"
-	"path/filepath"
 	"sync"
 )
 
 var (
-	defaultConfigPath = ".sparkai/config.yaml"
+	defaultConfigPath = "_config.yaml"
 )
+
+func pathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+func createFile(path string) error {
+	// 检查是否存在文件
+	exists, existsErr := pathExists(defaultConfigPath)
+	if exists {
+		return existsErr
+	}
+
+	f, createErr := os.Create(path)
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+		}
+	}(f)
+
+	return createErr
+}
 
 type config struct {
 }
@@ -20,34 +48,45 @@ var once sync.Once
 func GetInstance() *config {
 	if instance == nil {
 		once.Do(func() {
-			instance = &config{}
+			err := InitConfig()
+			if err != nil {
+				fmt.Println("init config error: ", err.Error())
+				return
+			}
 		})
 	}
 	return instance
 }
 
-func fileFullPath(relativeFilePath string) (string, error) {
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		return "", fmt.Errorf("error getting home directory: %s", err)
-	}
-	configFilePath := filepath.Join(homeDir, relativeFilePath)
-	return configFilePath, nil
-}
-
 // InitConfig init config
 func InitConfig() error {
+	// 创建配置文件
+	if err := createFile(defaultConfigPath); err != nil {
+		return err
+	}
 
+	// 设置配置文件
+	viper.SetConfigFile(defaultConfigPath)
+	if err := viper.ReadInConfig(); err != nil {
+		return err
+	}
+
+	// todo 初始化基础配置
+	// todo viper.Set("mysql.url", "127.0.0.1")
+
+	instance = &config{}
+	// 写入
 	return nil
 }
 
 // Set config with key
 func Set(key, value string) {
-
+	_ = GetInstance()
+	viper.Set(key, value)
 }
 
 // Get config value with key
-func Get(key string) error {
-
-	return nil
+func Get(key string) any {
+	_ = GetInstance()
+	return viper.Get(key)
 }
